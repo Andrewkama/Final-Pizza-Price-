7a4f96320d01f257a4c55c1a95e126dc9b541e75
Andrewkamal <kamaa0880@wrdsb.ca>
2020-09-18 02:24:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,65 @@
+/**
+ 	*
+ 	* @author
+ 	*/
+       import java.util.Scanner;
+ 
+  
+    /**
+ 	* asks the customer  for the diameter of pizza in Cm, calculates the price  and sends the user a message.
+ 	* @author Andrew
+ 	*/
+    public class Main {
+    
+ 
+ 	/**
+   	* @param args the command line arguments
+ 	*/
+ 	public static void main(String[] args) {
+ 	
+ 	Scanner input = new Scanner(System.in);
+    
+ 
+ 	// varying components
+ 	double diameter; 
+ 	double subtotal; 
+ 	double tax; 
+ 	double total; 
+    
+ 
+ 	// tax
+ 	final double SALES_TAX = 0.13;
+ 	// Adtional fees
+ 	final double PRICE_LABOUR = 0.75; 
+ 	final double PRICE_POWER = 0.99;
+ 	final double PRICE_SIZE = 0.5; 
+ 	//Size range
+ 	final double SIZE_20 = 20; // medium
+ 	final double SIZE_40 = 40; //large
+   	//asks for  diameter 
+ 	System.out.println("What is the size of the pizza (in cm):");
+ 	diameter = input.nextDouble();    
+ 
+ 	// price for the pizza labour and power and size
+ 	subtotal = PRICE_LABOUR + PRICE_POWER + (diameter*PRICE_SIZE); 
+    //Finds tax amount
+ 	tax = subtotal*SALES_TAX;
+    //add tax to price 
+ 	total = subtotal + tax;
+    // display pirceing before tax , tax total and all togather
+   // price before tax 
+ 	System.out.println("Subtotal: $" + subtotal);
+    // taxe toatal
+ 	System.out.println("Taxes: $" + tax);
+    // total
+ 	System.out.println("Total: $" + total);
+   //add message 
+ 	if (diameter > SIZE_40){ // 40cm or larger pizza message
+ 	System.out.println("Whoa, big pizza! You might need a truck to get this home!");
+ 	}else if (diameter > SIZE_20){ //40cm - 20cm medium pizza message
+ 	System.out.println("This will be delicious!");
+ 	}else{ //20cm or less small pizza message
+ 	System.out.println("We are going to make you a cute little pizza!");
+ 	}
+ 	}
+    }
